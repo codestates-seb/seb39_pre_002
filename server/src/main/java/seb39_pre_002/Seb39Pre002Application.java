88643cb77081bc19ff515ae20d102886b6c30ea5
kclown0 <keumdy0@gmail.com>
2022-08-25 11:39:45 +0900

    test5

---
+++ server/src/main/java/seb39_pre_002/Seb39Pre002Application.java
@@ -0,0 +1,13 @@
+package seb39_pre_002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb39Pre002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb39Pre002Application.class, args);
+	}
+
+}
