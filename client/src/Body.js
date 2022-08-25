d39decd998d2c8d5e16a074cbe6e49f55f0f2e4b
RyuJiChang <rgc0582@gmail.com>
2022-08-25 14:19:54 +0900

    raw components

---
+++ client/src/Body.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+function Body() {
+  return <div></div>;
+}
+
+export default Body;
---
+++ client/src/Header.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+function header() {
+  return <div></div>;
+}
+
+export default header;
