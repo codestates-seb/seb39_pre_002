dd3cfba5ae172e3b3c7f6ee911a07b3affad7fae
RyuJiChang <rgc0582@gmail.com>
2022-08-30 16:11:48 +0900

    router clear

--- client/src/QuestionDetail.js
+++ client/src/QuestionDetail.js
@@ -1,104 +1,106 @@
 import styled from "styled-components";
 import { Link, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import Sidebar from "./Sidebar";
 
+const Body = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Div = styled.div`
+  .body {
+  }
+  display: block;
 
+  margin: 50px 50px 50px 100px;
+  text-align: left;
+  max-width: 800px;
 
+  label {
+    font-size: 20px;
+    display: block;
+    margin: 50px 20px 10px 0px;
+  }
 
-    const Div = styled.div`   
+  textarea {
+    width: 90%;
+    height: 200px;
+    padding: 6px 10px;
+    display: block;
+  }
 
-        display: block;
+  span {
+    font-size: 12px;
+    margin-right: 15px;
+  }
 
-        margin: 50px 50px 50px 100px;
-        text-align: left;
-        max-width: 800px;
-        
-    
-        label {
-            font-size: 20px;
-            display:block;
-            margin: 50px 20px 10px 0px;
-        }
+  section {
+    line-height: 150%;
+  }
+`;
 
-        textarea {
-            width: 90%;
-            height: 200px;
-            padding: 6px 10px;
-            display:block;
-        }
+const PostButton = styled.button`
+  color: white;
+  background-color: hsl(206, 100%, 52%);
+  border: 0;
+  border-radius: 3px;
+  padding: 8px;
+  cursor: pointer;
+  margin-top: 50px;
+`;
 
-        span {
-            font-size: 12px;
-            margin-right: 15px;
-        }           
-
-        section {
-            line-height: 150%;
-        }
-
-        
-    `;
-
-    const PostButton = styled.button`
-        color:white;
-        background-color:hsl(206,100%,52%);
-        border: 0;
-        border-radius: 3px;
-        padding: 8px;
-        cursor: pointer;
-        margin-top: 50px;
-    `;
-
-    const EditDeleteButton = styled.button`
-        border: none;
-        background-color: white;
-        color: grey;
-        margin-top: 30px;
-        cursor: pointer;
-    `;
-
-
+const EditDeleteButton = styled.button`
+  border: none;
+  background-color: white;
+  color: grey;
+  margin-top: 30px;
+  cursor: pointer;
+`;
 
 const QuestionDetail = () => {
+  const { id } = useParams();
+  const { data, error } = useFetch("http://localhost:3001/data/" + id);
 
-    const {id} = useParams();
-    const {data, error} = useFetch('http://localhost:3001/data/'+ id)
+  const question_views = 0;
 
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth();
+  let date = now.getDate();
 
+  return (
+    <>
+      {error && <div>{error}</div>}
+      {data && (
+        <Body>
+          <Sidebar />
+          <Div>
+            <h2>{data.title}</h2>
+            <p>
+              <span>
+                Asked <strong>{`${year}/${month}/${date}`}</strong>
+              </span>
+              <span>
+                Modified <strong>{`${year}/${month}/${date}`}</strong>
+              </span>
+              <span>
+                viewed <strong>{question_views}</strong>
+              </span>
+            </p>
+            <hr></hr>
+            <section>{data.content}</section>
+            <Link to="/modify">
+              <EditDeleteButton>Edit</EditDeleteButton>
+            </Link>
+            <EditDeleteButton>Delete</EditDeleteButton>
+            <label>Your Answer</label>
+            <textarea></textarea>
+            <PostButton>Post Your Answer</PostButton>
+          </Div>
+        </Body>
+      )}
+    </>
+  );
+};
 
-        const question_views = 0
-  
-        let now = new Date();
-        let year = now.getFullYear();
-        let month = now.getMonth();
-        let date = now.getDate();   
-  
-  
-      return (
-          <>
-            {error && <div>{error}</div>}
-            {data && (               
-                    <Div>            
-                        <h2>{data.title}</h2>
-                        <p>
-                            <span>Asked <strong>{`${year}/${month}/${date}`}</strong></span>
-                            <span>Modified <strong>{`${year}/${month}/${date}`}</strong></span>
-                            <span>viewed <strong>{question_views}</strong></span>
-                        </p>
-                        <hr></hr>
-                        <section>
-                            {data.content}
-                        </section>      
-                        <Link to="/modify"><EditDeleteButton>Edit</EditDeleteButton></Link>
-                        <EditDeleteButton>Delete</EditDeleteButton>
-                        <label>Your Answer</label>
-                            <textarea></textarea>
-                        <PostButton>Post Your Answer</PostButton>
-                    </Div>               
-            )}
-          </>
-      )
-  
-  }
- 
 export default QuestionDetail;
